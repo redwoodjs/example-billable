080d0b12a00a1efe8ae94848c4c3680052bb6505
justinvdm <justinvderm@gmail.com>
2025-04-28 11:44:56 +0200

    Upgrade to .76

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -2,20 +2,18 @@ import stylesUrl from "./style.css?url";
 
 import type { DocumentProps } from "@redwoodjs/sdk/router";
 
-export const Document: React.FC<DocumentProps> = ({
-  children,
-}) => (
+export const Document: React.FC<DocumentProps> = ({ children }) => (
   <html lang="en">
     <head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>Billable: Billing Made Simple. Period.</title>
       <link rel="stylesheet" href={stylesUrl} />
-      <link rel="preload" href="/src/client.tsx" as="script" />
+      <link rel="modulepreload" href="/src/client.tsx" as="script" />
     </head>
     <body>
       <div id="root">{children}</div>
-      <script src="/src/client.tsx"></script>
+      <script>import("/src/client.tsx")</script>
     </body>
   </html>
 );
